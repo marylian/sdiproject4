117f592aaa3a16d10c25678dff546e29c74b2693
Marylia Nieves <marylian@fullsail.edu>
2012-05-22 20:37:43 -0400

    Added a phone number validation function.

--- Nieves_Marylia_assignment4.js
+++ Nieves_Marylia_assignment4.js
@@ -1,1 +1,16 @@
-alert("JavaScript works!");
+validateNumber=function(phoneNumber){ //Procedure without any return.
+if (phoneNumber.length==12){
+//    console.log ("The phone number you have entered is invalid. Please retry");
+    if (phoneNumber.charAt(3)=="-"){
+//        console.log("The format of the phone number is invalid. Please retry")
+        if (phoneNumber.charAt(7)=="-"){
+            console.log("The format of your phone number is correct."); //Output if condition is met.
+        }
+    }
+}    
+else {
+    console.log("The format of the number is invalid. Please retry."); //Output if condition is not met.
+    }
+};
+var phoneNumber="407-276-1427";
+validateNumber(phoneNumber);
