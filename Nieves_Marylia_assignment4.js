68ee5ecb9bb2f058608ab905f086909cf51caf89
Marylia Nieves <marylian@fullsail.edu>
2012-05-22 21:00:38 -0400

    Added a email address validation function.

--- Nieves_Marylia_assignment4.js
+++ Nieves_Marylia_assignment4.js
@@ -1,8 +1,6 @@
 validateNumber=function(phoneNumber){ //Procedure without any return.
 if (phoneNumber.length==12){
-//    console.log ("The phone number you have entered is invalid. Please retry");
     if (phoneNumber.charAt(3)=="-"){
-//        console.log("The format of the phone number is invalid. Please retry")
         if (phoneNumber.charAt(7)=="-"){
             console.log("The format of your phone number is correct."); //Output if condition is met.
         }
@@ -12,5 +10,22 @@ else {
     console.log("The format of the number is invalid. Please retry."); //Output if condition is not met.
     }
 };
+var indexAt;
+var indexDot;
+
+validateEmail=function(emailAddress){
+	indexAt=emailAddress.indexOf("@");
+	indexDot=emailAddress.lastIndexOf(".");
+	if (indexAt>indexDot){
+		console.log("The email address you have entered is incorrect");
+	}
+	else{
+		console.log("The email address you have entered is correct. Please proceed");
+	}
+};
+
 var phoneNumber="407-276-1427";
 validateNumber(phoneNumber);
+emailAddress="p.utsav@gmail.com";
+validateEmail(emailAddress);
+
