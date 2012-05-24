c4e942991554cf9fd1fc7bba48d97b7efe960edb
Marylia Nieves <marylian@fullsail.edu>
2012-05-23 21:32:35 -0400

    Testing email verification function with a different email address

--- Nieves_Marylia_assignment4.js
+++ Nieves_Marylia_assignment4.js
@@ -1,28 +1,29 @@
-validateNumber=function(phoneNumber){ //Procedure without any return.
-if (phoneNumber.length==12){
-    if (phoneNumber.charAt(3)=="-"){
-        if (phoneNumber.charAt(7)=="-"){
-            console.log("The format of your phone number is correct."); //Output if condition is met.
+
+validateNumber=function(phoneNumber){ //Function to validate the phone number format.
+if (phoneNumber.length==12){		  //Checks for the total length of the phone number.
+    if (phoneNumber.charAt(3)=="-"){  //Checks if there is a hyphen after the first three numbers.
+        if (phoneNumber.charAt(7)=="-"){ //Checks if there is another hyphen after the first 6 numbers.
+            console.log("The format of your phone number is correct."); //Output after all the conditions are met.
         }
     }
 }    
 else {
-    console.log("The format of the number is invalid. Please retry."); //Output if condition is not met.
+    console.log("The format of the number is invalid. Please retry."); // Output if any of the conditions are not met.
     }
 };
 
 
 
-validateEmail=function(emailAddress){
-	var specialChar=["!", "@", "#", "@", "$", "%", "^", "&", "*", "(", ")", "<", ">", "?", "/", "{", "}", "+", "=", "`", "~", "|"]; 
+validateEmail=function(emailAddress){  //Function to validate the email address format.
+	var specialChar=["!", "@", "#", "@", "$", "%", "^", "&", "*", "(", ")", "<", ">", "?", "/", "{", "}", "+", "=", "`", "~", "|"];   //Array of special characters for which the function will check against.
 	var compEmail;
 	var emailVal;
 	var indexAt=emailAddress.indexOf("@");
 	var indexDot=emailAddress.lastIndexOf(".");
 	compEmail=emailAddress.substring (0, indexAt);
-	for (var i=0; i<=specialChar.length; i++){
+	for (var i=0; i<=specialChar.length; i++){  //First for loop to check against special characters present before the "@" sign.
 		var j;
-		for (compEmail[j=0]; j<compEmail.length; j++){
+		for (compEmail[j=0]; j<compEmail.length; j++){  //Second for loop to check against special chacters present before the @ sign.
 			if (compEmail[j]==specialChar[i]){
 				console.log("Check for invalid special characters in your email address.");
 			}
@@ -39,7 +40,7 @@ validateEmail=function(emailAddress){
 };
 
 
-validateNum = function(decimalNum){
+validateNum = function(decimalNum){  //Function to format upto 2 decimal positions.
 	var decimalPos=decimalNum.indexOf(".");
 	var x=decimalNum.substring (0, decimalPos);
 	var z=decimalNum.indexOf(".");
@@ -50,7 +51,7 @@ validateNum = function(decimalNum){
 //	console.log(result);
 };
 
-convString = function(stringNum){
+convString = function(stringNum){  //Function to convert string number to an actual number.
 	var count=0;
 	for (numConv=0; numConv<stringNum.length; numConv++){
 		count++;
@@ -59,7 +60,7 @@ convString = function(stringNum){
 	console.log(numConv);
 };
 
-sumNum = function(mixedArray){
+sumNum = function(mixedArray){  //Function to add the numbers in a mixed array.
 	var sum=0;
 	for (i=0; i<=mixedArray.length; i++){
 			if (isNaN(mixedArray[i]%2)){
@@ -72,7 +73,7 @@ sumNum = function(mixedArray){
 	console.log(sum);
 };
 
-percentRadius = function(givenNum, constNum, percent){
+percentRadius = function(givenNum, constNum, percent){  //Function to check if a given number is above or below a certain percent of a number.
 	var convConstNum=(constNum*(percent/100));
 	convConstNum=(constNum+convConstNum);
 	if (givenNum<convConstNum){
@@ -86,7 +87,7 @@ percentRadius = function(givenNum, constNum, percent){
 
 var phoneNumber="407-276-1427";
 validateNumber(phoneNumber);
-var emailAddress="p.utsav@gmail.com";
+var emailAddress="marylia_n@yahoo.com";
 validateEmail(emailAddress);
 var decimalNum="11234.2347389372047";
 validateNum(decimalNum);
